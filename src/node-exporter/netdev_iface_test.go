5de84e40f03ad2c4518af2f17ce6050a0b315d34
phstsai <phstsai@caipeixuande-MacBook-Pro.local>
2018-05-28 13:56:52 +0800

    add unit test for src/node-exporter/netdev_iface.go

---
+++ src/node-exporter/netdev_iface_test.go
@@ -0,0 +1,47 @@
+package collector
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+func TestGetDefaultDev(t *testing.T) {
+	defaultInterface := ""
+	switch runtime.GOOS {
+	case "linux":
+		out, err := exec.Command("bash", "-c", "ip route get 8.8.8.8 | awk 'NR==1 {print $5}'").Output()
+		if err != nil {
+			t.Error("Can't get the interface for default route, maybe the system does not have the network connectivity", err)
+		}
+		//the output should look like eth0
+		fmt.Sscanf(string(out), "%s", &defaultInterface)
+		if defaultInterface == "" {
+			t.Error("Parse the interface fail")
+		}
+
+	case "darwin":
+		//
+		t.Skip("Skip the test when os is drawin.")
+
+	}
+
+	iface := ""
+
+	netDevDefault, err := getDefaultDev()
+	if err != nil {
+		t.Error("couldn't get default network devices: %s", err)
+	}
+
+	for key, value := range netDevDefault {
+		if value == 1 {
+			iface = key
+		}
+	}
+
+	if strings.Compare(defaultInterface, iface) != 0 {
+		t.Error("Default interface didn't match the result from getDefaultDev().")
+	}
+}
