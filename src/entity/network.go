6456a192e3c459940f16999f8379c807bcd8b95c
Pei Hsuan Tsai <phstsai@linkernetworks.com>
2018-06-07 14:10:12 +0800

    add network entity

---
+++ src/entity/network.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Network struct {
+	ID          bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	DisplayName string        `bson:"displayName" json:"displayName"`
+	BridgeName  string        `bson:"bridgeName" json:"bridgeName"`
+	BridgeType  string        `bson:"bridgeType" json:"bridgeType"`
+	Node        []string      `bson:"node" json:"node"`
+	Interface   string        `bson:"interface" json:"interface"`
+	Ports       []int32       `bson:"ports" json:"ports"`
+	MTU         int32         `bson:"maximumTransmissionUnit" MTC:"maximumTransmissionUnit"`
+}
