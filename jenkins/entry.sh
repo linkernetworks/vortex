84b8678c93c554797968cec66c3e67b03cfb93e6
Aren Chen <arenchen@linkernetworks.com>
2018-06-15 23:09:51 +0800

    test Jenkinsfile

--- jenkins/entry.sh
+++ jenkins/entry.sh
@@ -9,6 +9,7 @@ echo 'jenkins:x:'$UID':'$GID':,,,:/home/jenkins:/bin/bash' >> /etc/passwd
 sudo service docker start
 mkdir -p /home/jenkins/data/mongo
 nohup mongod --dbpath=/home/jenkins/data/mongo 2>&1 > /dev/null &
+#mongod --dbpath=/home/jenkins/data/mongo
 
 sudo chown -R jenkins:jenkins /home/jenkins
 
