de1c5c4a76544a9f72bbe4b8e0542ecec6e7c004
yunchen <me@yunchen.tw>
2018-06-21 07:38:27 +0000

    Add fake clientset for k8s testing

--- src/kubernetes/nodes.go
+++ src/kubernetes/nodes.go
@@ -1,17 +1,17 @@
 package kubernetes
 
 import (
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-func GetNode(clientset *kubernetes.Clientset, name string) (*v1.Node, error) {
+func GetNode(clientset kubernetes.Interface, name string) (*corev1.Node, error) {
 	return clientset.CoreV1().Nodes().Get(name, metav1.GetOptions{})
 }
 
-func GetNodes(clientset *kubernetes.Clientset) ([]*v1.Node, error) {
-	nodes := []*v1.Node{}
+func GetNodes(clientset kubernetes.Interface) ([]*corev1.Node, error) {
+	nodes := []*corev1.Node{}
 	nodesList, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
 	if err != nil {
 		return nodes, err
