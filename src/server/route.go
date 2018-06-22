0d70fb44f21441e2bbb19ccffdd1bb75720fff81
Pei Hsuan Tsai <phstsai@linkernetworks.com>
2018-06-22 17:44:27 +0800

    fix typo

--- src/server/route.go
+++ src/server/route.go
@@ -18,7 +18,7 @@ func (a *App) AppRoute() *mux.Router {
 	container.Add(newNetworkService(a.ServiceProvider))
 	container.Add(newStorageProviderService(a.ServiceProvider))
 	container.Add(newVolumeService(a.ServiceProvider))
-	container.Add(newMonirotingService(a.ServiceProvider))
+	container.Add(newMonitoringService(a.ServiceProvider))
 
 	router.PathPrefix("/v1/").Handler(container)
 	return router
@@ -57,7 +57,7 @@ func newVolumeService(sp *serviceprovider.Container) *restful.WebService {
 	return webService
 }
 
-func newMonirotingService(sp *serviceprovider.Container) *restful.WebService {
+func newMonitoringService(sp *serviceprovider.Container) *restful.WebService {
 	webService := new(restful.WebService)
 	webService.Path("/v1/monitoring").Consumes(restful.MIME_JSON, restful.MIME_JSON).Produces(restful.MIME_JSON, restful.MIME_JSON)
 	webService.Route(webService.GET("/query").To(handler.RESTfulServiceHandler(sp, QueryMetrics)))
