cd2d5aa7c047eabd8f87b96589565d3e8643530e
John-Lin <linton.tw@gmail.com>
2018-06-23 22:06:40 +0800

    add NewKubeCtl test

---
+++ src/kubernetes/kubectl_test.go
@@ -0,0 +1,17 @@
+package kubernetes
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	fakeclientset "k8s.io/client-go/kubernetes/fake"
+)
+
+func TestNewKubeCtl(t *testing.T) {
+	clientset := fakeclientset.NewSimpleClientset()
+	namespace := "default"
+	kubectl := New(clientset, namespace)
+	assert.Equal(t, namespace, kubectl.Namespace)
+	assert.NotNil(t, kubectl)
+}
