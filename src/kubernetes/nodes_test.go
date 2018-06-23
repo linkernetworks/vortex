27e8195b32e83c1826314533d73d080bc96ad98c
John-Lin <linton.tw@gmail.com>
2018-06-23 17:55:23 +0800

    support to get node IP

--- src/kubernetes/nodes_test.go
+++ src/kubernetes/nodes_test.go
@@ -1,11 +1,12 @@
 package kubernetes
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	fakeclientset "k8s.io/client-go/kubernetes/fake"
-	"testing"
 )
 
 func TestGetNode(t *testing.T) {
@@ -53,4 +54,26 @@ func TestGetNodes(t *testing.T) {
 	nodes, err := GetNodes(clientset)
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(nodes))
+}
+
+func TestGetNodeExternalIP(t *testing.T) {
+	clientset := fakeclientset.NewSimpleClientset()
+	nodeAddr := corev1.NodeAddress{
+		Type:    "ExternalIP",
+		Address: "192.168.0.100",
+	}
+	node := corev1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "K8S-Node",
+		},
+		Status: corev1.NodeStatus{
+			Addresses: []corev1.NodeAddress{nodeAddr},
+		},
+	}
+	_, err := clientset.CoreV1().Nodes().Create(&node)
+	assert.NoError(t, err)
+
+	nodeIP, err := GetNodeExternalIP(clientset, "K8S-Node")
+	assert.NoError(t, err)
+	assert.Equal(t, nodeAddr.Address, nodeIP)
 }
