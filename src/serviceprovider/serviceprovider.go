2207953a7d5c14a2233c1d3539805e5510afed3b
hwchiu <hwchiu@linkernetworks.com>
2018-06-26 17:23:13 +0800

    Use the NewForTest for fake client testing

--- src/serviceprovider/serviceprovider.go
+++ src/serviceprovider/serviceprovider.go
@@ -12,6 +12,7 @@ import (
 	"github.com/linkernetworks/mongo"
 	"github.com/linkernetworks/redis"
 	kubeCtl "github.com/linkernetworks/vortex/src/kubernetes"
+
 	"k8s.io/client-go/kubernetes"
 	fakeclientset "k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
@@ -53,13 +54,39 @@ func New(cf config.Config) *Container {
 
 	clientset := kubernetes.NewForConfigOrDie(k8s)
 
-	var clientset kubernetes.Interface
-
-	if cf.Kubernetes != nil {
-		clientset = kubernetes.NewForConfigOrDie(cf.Kubernetes)
-	} else {
-		clientset = fakeclientset.NewSimpleClientset()
+	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	k8s, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		k8s, err = rest.InClusterConfig()
+		if err != nil {
+			panic(fmt.Errorf("Load the kubernetes config fail"))
+		}
 	}
+
+	clientset := kubernetes.NewForConfigOrDie(k8s)
+
+	sp := &Container{
+		Config:     cf,
+		Redis:      redisService,
+		Mongo:      mongo,
+		Prometheus: prometheus,
+		KubeCtl:    kubeCtl.New(clientset, "default"),
+	}
+
+	return sp
+}
+
+func NewForTesting(cf config.Config) *Container {
+	// setup logger configuration
+	logger.Setup(cf.Logger)
+
+	logger.Infof("Connecting to redis: %s", cf.Redis.Addr())
+	redisService := redis.New(cf.Redis)
+
+	logger.Infof("Connecting to mongodb: %s", cf.Mongo.Url)
+	mongo := mongo.New(cf.Mongo.Url)
+
+	clientset := fakeclientset.NewSimpleClientset()
 
 	sp := &Container{
 		Config:  cf,
