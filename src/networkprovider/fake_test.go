f284751f2ba40e21c2c643160afc91c3c13f47fe
hwchiu <hwchiu@linkernetworks.com>
2018-06-28 16:20:09 +0800

    Add testing for network provider

---
+++ src/networkprovider/fake_test.go
@@ -0,0 +1,58 @@
+package networkprovider
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/linkernetworks/vortex/src/entity"
+)
+
+func TestFakeNetworkValidateBeforeCreating(t *testing.T) {
+	fake, err := GetNetworkProvider(&entity.Network{
+		Type: "fake",
+		Fake: entity.FakeNetwork{
+			FakeParameter: "yes",
+		},
+	})
+	assert.NoError(t, err)
+	err = fake.CreateNetwork(nil, entity.Network{})
+	assert.NoError(t, err)
+}
+
+func TestFakeNetworkCreating(t *testing.T) {
+	fake, err := GetNetworkProvider(&entity.Network{
+		Type: "fake",
+		Fake: entity.FakeNetwork{
+			FakeParameter: "yes",
+			IWantFail:     false,
+		},
+	})
+	assert.NoError(t, err)
+	err = fake.CreateNetwork(nil, entity.Network{})
+	assert.NoError(t, err)
+}
+
+func TestFakeNetworkValidateBeforeCreatingFail(t *testing.T) {
+	fake, err := GetNetworkProvider(&entity.Network{
+		Type: "fake",
+		Fake: entity.FakeNetwork{
+			FakeParameter: "",
+		},
+	})
+	assert.NoError(t, err)
+	err = fake.ValidateBeforeCreating(nil, entity.Network{})
+	assert.Error(t, err)
+}
+
+func TestFakeNetworkCreatingFail(t *testing.T) {
+	fake, err := GetNetworkProvider(&entity.Network{
+		Type: "fake",
+		Fake: entity.FakeNetwork{
+			IWantFail: true,
+		},
+	})
+	assert.NoError(t, err)
+	err = fake.CreateNetwork(nil, entity.Network{})
+	assert.Error(t, err)
+}
