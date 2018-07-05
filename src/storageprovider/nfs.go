b73a4925c45260c61f243fe78cd87baaf3680bad
hwchiu <hwchiu@linkernetworks.com>
2018-07-05 02:54:43 +0000

    Add the validata function

--- src/storageprovider/nfs.go
+++ src/storageprovider/nfs.go
@@ -1,8 +1,10 @@
 package storageprovider
 
 import (
+	"fmt"
 	"github.com/linkernetworks/vortex/src/entity"
 	"github.com/linkernetworks/vortex/src/serviceprovider"
+	"net"
 	//	"gopkg.in/mgo.v2/bson"
 )
 
@@ -10,14 +12,24 @@ type NFSStorageProvider struct {
 	entity.NFSStorage
 }
 
-func (nfs NFSStorageProvider) ValidateBeforeCreating(sp *serviceprovider.Container, network entity.Storage) error {
+func (nfs NFSStorageProvider) ValidateBeforeCreating(sp *serviceprovider.Container, storage entity.Storage) error {
+	ip := net.ParseIP(storage.NFS.IP)
+	if len(ip) == 0 {
+		return fmt.Errorf("Invalid IP address %s\n", storage.NFS.IP)
+	}
+
+	path := storage.NFS.PATH
+	if path == "" || path[0] != '/' {
+		return fmt.Errorf("Invalid NFS export path %s\n", path)
+	}
+
 	return nil
 }
 
-func (nfs NFSStorageProvider) CreateStorage(sp *serviceprovider.Container, network entity.Storage) error {
+func (nfs NFSStorageProvider) CreateStorage(sp *serviceprovider.Container, storage entity.Storage) error {
 	return nil
 }
 
-func (nfs NFSStorageProvider) DeleteStorage(sp *serviceprovider.Container, network entity.Storage) error {
+func (nfs NFSStorageProvider) DeleteStorage(sp *serviceprovider.Container, storage entity.Storage) error {
 	return nil
 }
