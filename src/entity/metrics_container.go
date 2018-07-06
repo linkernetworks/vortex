486f0555a682905a91212b514b04c5028a8f387c
Pei Hsuan Tsai <phstsai@linkernetworks.com>
2018-07-06 17:30:12 +0800

    revert mongo statefulset YAML

--- src/entity/metrics_container.go
+++ src/entity/metrics_container.go
@@ -1,14 +1,5 @@
 package entity
 
 type ContainerMetrics struct {
-	ContainerName string            `json:"containerName"`
-	Namespace     string            `json:"namespace"`
-	Node          string            `json:"node"`
-	Status        string            `json:"status"`
-	CreateAt      int               `json:"createAt"`
-	CreateByKind  string            `json:"createByKind"`
-	CreateByName  string            `json:"createByName"`
-	IP            string            `json:"ip"`
-	Labels        map[string]string `json:"labels"`
-	RestartCount  int               `json:"restartCount"`
+	ContainerName string `json:"containerName"`
 }
