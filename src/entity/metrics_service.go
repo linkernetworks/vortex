6c2d275702df5ffe62f8330538d6b8f733195513
Pei Hsuan Tsai <phstsai@linkernetworks.com>
2018-07-09 18:14:39 +0800

    use client-go get service ports

--- src/entity/metrics_service.go
+++ src/entity/metrics_service.go
@@ -1,10 +1,15 @@
 package entity
 
+import (
+	"k8s.io/api/core/v1"
+)
+
 type ServiceMetrics struct {
 	ServiceName string            `json:"serviceName"`
 	Namespace   string            `json:"namespace"`
 	Type        string            `json:"type"`
 	CreateAt    int               `json:"createAt"`
 	ClusterIP   string            `json:"clusterIP"`
+	Ports       []v1.ServicePort  `json: "ports"`
 	Labels      map[string]string `json:"labels"`
 }
