04f1248f8873be12e3d99615bad3a0f835d7557a
hwchiu <hwchiu@linkernetworks.com>
2018-07-11 03:09:39 +0000

    Add testing for pod

--- src/pod/pod_test.go
+++ src/pod/pod_test.go
@@ -33,6 +33,85 @@ func TestPodSuite(t *testing.T) {
 	suite.Run(t, new(PodTestSuite))
 }
 
+func (suite *PodTestSuite) TestCheckPodParameter() {
+	volumeName := namesgenerator.GetRandomName(0)
+	pod := &entity.Pod{
+		ID: bson.NewObjectId(),
+		Volumes: []entity.PodVolume{
+			{Name: volumeName},
+		},
+	}
+
+	session := suite.sp.Mongo.NewSession()
+	defer session.Close()
+
+	volume := entity.Volume{
+		ID:   bson.NewObjectId(),
+		Name: volumeName,
+	}
+
+	session.Insert(entity.VolumeCollectionName, volume)
+	defer session.Remove(entity.VolumeCollectionName, "name", volume.Name)
+
+	err := CheckPodParameter(suite.sp, pod)
+	suite.NoError(err)
+}
+
+func (suite *PodTestSuite) TestCheckPodParameterFail() {
+	volumeName := namesgenerator.GetRandomName(0)
+	pod := &entity.Pod{
+		ID: bson.NewObjectId(),
+		Volumes: []entity.PodVolume{
+			{Name: volumeName},
+		},
+	}
+
+	err := CheckPodParameter(suite.sp, pod)
+	suite.Error(err)
+}
+
+func (suite *PodTestSuite) TestGenerateVolume() {
+	volumeName := namesgenerator.GetRandomName(0)
+	pod := &entity.Pod{
+		ID: bson.NewObjectId(),
+		Volumes: []entity.PodVolume{
+			{Name: volumeName},
+		},
+	}
+
+	session := suite.sp.Mongo.NewSession()
+	defer session.Close()
+
+	volume := entity.Volume{
+		ID:   bson.NewObjectId(),
+		Name: volumeName,
+	}
+	session.Insert(entity.VolumeCollectionName, volume)
+	defer session.Remove(entity.VolumeCollectionName, "name", volume.Name)
+
+	volumes, volumeMounts, err := generateVolume(pod, session)
+	suite.NotNil(volumes)
+	suite.NotNil(volumeMounts)
+	suite.NoError(err)
+}
+
+func (suite *PodTestSuite) TestGenerateVolumeFail() {
+	volumeName := namesgenerator.GetRandomName(0)
+	pod := &entity.Pod{
+		ID: bson.NewObjectId(),
+		Volumes: []entity.PodVolume{
+			{Name: volumeName},
+		},
+	}
+
+	session := suite.sp.Mongo.NewSession()
+	defer session.Close()
+	volumes, volumeMounts, err := generateVolume(pod, session)
+	suite.Nil(volumes)
+	suite.Nil(volumeMounts)
+	suite.Error(err)
+}
+
 func (suite *PodTestSuite) TestCreatePod() {
 	containers := []entity.Container{
 		{
@@ -54,4 +133,27 @@ func (suite *PodTestSuite) TestCreatePod() {
 
 	err = DeletePod(suite.sp, podName)
 	suite.NoError(err)
+}
+
+func (suite *PodTestSuite) TestCreatePodFail() {
+	containers := []entity.Container{
+		{
+			Name:    namesgenerator.GetRandomName(0),
+			Image:   "busybox",
+			Command: []string{"sleep", "3600"},
+		},
+	}
+
+	podName := namesgenerator.GetRandomName(0)
+	pod := &entity.Pod{
+		ID:         bson.NewObjectId(),
+		Name:       podName,
+		Containers: containers,
+		Volumes: []entity.PodVolume{
+			{Name: namesgenerator.GetRandomName(0)},
+		},
+	}
+
+	err := CreatePod(suite.sp, pod)
+	suite.Error(err)
 }
