fba04ae202325811b8d1b65acd4e5d2b1189d69e
hwchiu <hwchiu@linkernetworks.com>
2018-07-12 04:00:47 +0000

    Add new function to check dose the pod has already completed

--- src/kubernetes/pods.go
+++ src/kubernetes/pods.go
@@ -33,3 +33,13 @@ func (kc *KubeCtl) DeletePod(name string) error {
 	options := metav1.DeleteOptions{}
 	return kc.Clientset.CoreV1().Pods(kc.Namespace).Delete(name, &options)
 }
+
+func (kc *KubeCtl) DoesPodCompleted(pod *corev1.Pod) bool {
+	switch pod.Status.Phase {
+	case corev1.PodRunning, corev1.PodPending:
+		return false
+	default:
+		return true
+	}
+	return true
+}
