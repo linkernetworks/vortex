4d839739f524b0f16dcfcda72980496da1dd3ef0
hwchiu <hwchiu@linkernetworks.com>
2018-07-13 07:48:44 +0000

    Add the function to get the intersection of arrays

---
+++ src/utils/intersection.go
@@ -0,0 +1,29 @@
+package utils
+
+func Intersection(a, b []string) (ret []string) {
+	m := make(map[string]bool)
+
+	for _, item := range a {
+		m[item] = true
+	}
+
+	for _, item := range b {
+		if _, ok := m[item]; ok {
+			ret = append(ret, item)
+		}
+	}
+	return
+}
+
+func Intersections(input [][]string) (ret []string) {
+	if len(input) == 0 {
+		return
+	}
+
+	ret = input[0]
+	for i := 1; i < len(input); i++ {
+		ret = Intersection(ret, input[i])
+	}
+
+	return
+}
