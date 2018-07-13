92b3007b12f2c58e2a69dba0ca39f1a2bc4caa2d
John-Lin <linton.tw@gmail.com>
2018-07-13 16:18:04 +0800

    typos

--- src/utils/utils_test.go
+++ src/utils/utils_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGenerateVethName(t *testing.T) {
+func TestSHA256String(t *testing.T) {
 	o := SHA256String("12345678")
 	assert.Equal(t, "ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f", o)
 }
