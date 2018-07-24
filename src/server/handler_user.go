ea3bfec401df2fba84d0fed97b948a13af1fb854
John-Lin <linton.tw@gmail.com>
2018-07-24 17:23:30 +0800

    add user CRUD

---
+++ src/server/handler_user.go
@@ -0,0 +1,163 @@
+package server
+
+import (
+	"math"
+	"strconv"
+
+	"github.com/linkernetworks/utils/timeutils"
+	"github.com/linkernetworks/vortex/src/entity"
+	response "github.com/linkernetworks/vortex/src/net/http"
+	"github.com/linkernetworks/vortex/src/net/http/query"
+	"github.com/linkernetworks/vortex/src/web"
+
+	"github.com/satori/go.uuid"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func createUserHandler(ctx *web.Context) {
+	sp, req, resp := ctx.ServiceProvider, ctx.Request, ctx.Response
+
+	user := entity.User{}
+	if err := req.ReadEntity(&user); err != nil {
+		response.BadRequest(req.Request, resp.ResponseWriter, err)
+		return
+	}
+
+	uuid4, _ := uuid.NewV4()
+	user.UUID = uuid4
+	encryptedPassword, err := hashPassword(user.Password)
+	if err != nil {
+		response.InternalServerError(req.Request, resp.ResponseWriter, err)
+		return
+	}
+	user.Password = encryptedPassword
+
+	if err := sp.Validator.Struct(user); err != nil {
+		response.BadRequest(req.Request, resp.ResponseWriter, err)
+		return
+	}
+
+	session := sp.Mongo.NewSession()
+	defer session.Close()
+
+	user.ID = bson.NewObjectId()
+	user.CreatedAt = timeutils.Now()
+
+	if err := session.Insert(entity.UserCollectionName, &user); err != nil {
+		response.InternalServerError(req.Request, resp.ResponseWriter, err)
+		return
+	}
+
+	resp.WriteEntity(ActionResponse{
+		Error:   false,
+		Message: "User Created Success",
+	})
+}
+
+func deleteUserHandler(ctx *web.Context) {
+	sp, req, resp := ctx.ServiceProvider, ctx.Request, ctx.Response
+
+	id := req.PathParameter("id")
+
+	session := sp.Mongo.NewSession()
+	defer session.Close()
+
+	user := entity.User{}
+	if err := session.FindOne(
+		entity.UserCollectionName,
+		bson.M{"_id": bson.ObjectIdHex(id)},
+		&user,
+	); err != nil {
+		response.BadRequest(req.Request, resp.ResponseWriter, err)
+		return
+	}
+
+	if err := session.Remove(entity.UserCollectionName, "_id", bson.ObjectIdHex(id)); err != nil {
+		switch err {
+		case mgo.ErrNotFound:
+			response.NotFound(req.Request, resp.ResponseWriter, err)
+			return
+		default:
+			response.InternalServerError(req.Request, resp.ResponseWriter, err)
+			return
+		}
+	}
+
+	resp.WriteEntity(ActionResponse{
+		Error:   false,
+		Message: "User Deleted Success",
+	})
+}
+
+func listUserHandler(ctx *web.Context) {
+	sp, req, resp := ctx.ServiceProvider, ctx.Request, ctx.Response
+
+	var pageSize = 10
+	query := query.New(req.Request.URL.Query())
+
+	page, err := query.Int("page", 1)
+	if err != nil {
+		response.BadRequest(req.Request, resp.ResponseWriter, err)
+		return
+	}
+	pageSize, err = query.Int("page_size", pageSize)
+	if err != nil {
+		response.BadRequest(req.Request, resp.ResponseWriter, err)
+		return
+	}
+
+	session := sp.Mongo.NewSession()
+	defer session.Close()
+
+	users := []entity.User{}
+	var c = session.C(entity.UserCollectionName)
+	var q *mgo.Query
+
+	selector := bson.M{}
+	q = c.Find(selector).Sort("_id").Skip((page - 1) * pageSize).Limit(pageSize)
+
+	if err := q.All(&users); err != nil {
+		switch err {
+		case mgo.ErrNotFound:
+			response.NotFound(req.Request, resp.ResponseWriter, err)
+			return
+		default:
+			response.InternalServerError(req.Request, resp.ResponseWriter, err)
+			return
+		}
+	}
+
+	count, err := session.Count(entity.UserCollectionName, bson.M{})
+	if err != nil {
+		response.InternalServerError(req.Request, resp.ResponseWriter, err)
+		return
+	}
+	totalPages := int(math.Ceil(float64(count) / float64(pageSize)))
+	resp.AddHeader("X-Total-Count", strconv.Itoa(count))
+	resp.AddHeader("X-Total-Pages", strconv.Itoa(totalPages))
+	resp.WriteEntity(users)
+}
+
+func getUserHandler(ctx *web.Context) {
+	sp, req, resp := ctx.ServiceProvider, ctx.Request, ctx.Response
+
+	id := req.PathParameter("id")
+
+	session := sp.Mongo.NewSession()
+	defer session.Close()
+	c := session.C(entity.UserCollectionName)
+
+	user := entity.User{}
+	if err := c.FindId(bson.ObjectIdHex(id)).One(&user); err != nil {
+		switch err {
+		case mgo.ErrNotFound:
+			response.NotFound(req.Request, resp.ResponseWriter, err)
+			return
+		default:
+			response.InternalServerError(req.Request, resp.ResponseWriter, err)
+			return
+		}
+	}
+	resp.WriteEntity(user)
+}
