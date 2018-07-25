0d5d5a4a13650c6b94917b0696c2aacbabb48807
Pei Hsuan Tsai <phstsai@linkernetworks.com>
2018-07-25 15:16:00 +0800

    unit test for version handler

---
+++ src/server/handler_version_test.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	restful "github.com/emicklei/go-restful"
+
+	"github.com/linkernetworks/vortex/src/config"
+	"github.com/linkernetworks/vortex/src/serviceprovider"
+	"github.com/stretchr/testify/suite"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+type VersionTestSuite struct {
+	suite.Suite
+	wc *restful.Container
+	sp *serviceprovider.Container
+}
+
+func (suite *VersionTestSuite) SetupSuite() {
+	cf := config.MustRead("../../config/testing.json")
+	sp := serviceprovider.New(cf)
+
+	//init restful container
+	suite.wc = restful.NewContainer()
+	service := newVersionService(sp)
+	suite.wc.Add(service)
+}
+
+func (suite *VersionTestSuite) TearDownSuite() {}
+
+func TestVersionSuite(t *testing.T) {
+	suite.Run(t, new(VersionTestSuite))
+}
+
+func (suite *VersionTestSuite) TestVersion() {
+	httpRequest, err := http.NewRequest("GET", "http://localhost:7890/v1/version", nil)
+	suite.NoError(err)
+
+	httpWriter := httptest.NewRecorder()
+	suite.wc.Dispatch(httpWriter, httpRequest)
+	assertResponseCode(suite.T(), http.StatusOK, httpWriter)
+}
