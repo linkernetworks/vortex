5b9ff6e68be979b220e8a029d14ed64659cf0cd4
John-Lin <linton.tw@gmail.com>
2018-08-07 14:00:48 +0800

    registry basic auth proxy

---
+++ src/registry/registry.go
@@ -0,0 +1,6 @@
+package registry
+
+// Config is the structure for Registry Config
+type Config struct {
+	URL string `json:"url"`
+}
---
+++ src/server/handler_registry.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/linkernetworks/vortex/src/entity"
+	response "github.com/linkernetworks/vortex/src/net/http"
+	"github.com/linkernetworks/vortex/src/web"
+)
+
+func registryBasicAuthHandler(ctx *web.Context) {
+	sp, req, resp := ctx.ServiceProvider, ctx.Request, ctx.Response
+
+	credential := entity.RegistryBasicAuthCredential{}
+	if err := req.ReadEntity(&credential); err != nil {
+		response.BadRequest(req.Request, resp.ResponseWriter, err)
+		return
+	}
+
+	if err := sp.Validator.Struct(credential); err != nil {
+		response.BadRequest(req.Request, resp.ResponseWriter, err)
+		return
+	}
+
+	client := &http.Client{}
+	registryReq, err := http.NewRequest("GET", sp.Config.Registry.URL+"/v2", nil)
+	registryReq.SetBasicAuth(credential.Username, credential.Password)
+	registryResp, err := client.Do(registryReq)
+	if err != nil {
+		response.InternalServerError(req.Request, resp.ResponseWriter, err)
+		return
+	}
+	resp.WriteHeaderAndEntity(registryResp.StatusCode, credential)
+}
