a6bb958cb8c492a67f66f1de8aa34024137c7c4e
John-Lin <linton.tw@gmail.com>
2018-08-21 12:05:01 +0800

    add account/passwotd

--- tests/02-multiple-interface/init.bash
+++ tests/02-multiple-interface/init.bash
@@ -14,5 +14,5 @@ if [ -z "$networkName" ]; then
     sed -i  "s/@NETWORKNAME@/${networkName}/" pod.json
     sed -i  "s/@PODNAME@/${podName}/" pod.json
     # login
-    export JWT_AUTH_TOKEN=$(http --check-status http://127.0.0.1:7890/v1/users/signin < credential.json 2>/dev/null | jq ".message")
+    export JWT_AUTH_TOKEN=$(http --check-status http://127.0.0.1:7890/v1/users/signin < credential.json 2>/dev/null | jq -r ".message")
 fi
