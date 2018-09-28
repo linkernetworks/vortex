9912f5dc835242b7f46f69a36c40fd3bc5515ae2
Pei Hsuan Tsai <phstsai@linkernetworks.com>
2018-09-28 12:40:57 +0800

    address the comments

--- src/entity/configMap.go
+++ src/entity/configMap.go
@@ -17,7 +17,7 @@ type ConfigMap struct {
 	OwnerID   bson.ObjectId     `bson:"ownerID,omitempty" json:"ownerID" validate:"-"`
 	Name      string            `bson:"name" json:"name" validate:"required,k8sname"`
 	Namespace string            `bson:"namespace" json:"namespace" validate:"required"`
-	Data      map[string]string `json:"data,omitempty" protobuf:"bytes,2,rep,name=data"`
+	Data      map[string]string `bson:"data,omitempty" json:"data,omitempty" validate:"-"`
 	CreatedAt *time.Time        `bson:"createdAt,omitempty" json:"createdAt,omitempty" validate:"-"`
 	CreatedBy User              `json:"createdBy" validate:"-"`
 }
